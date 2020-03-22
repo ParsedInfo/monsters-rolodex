9a7517205c4f2b8711d3d3cfb962c5d9892c12bf
Jeff <jeff@parsed.info>
2020-03-22 15:11:12 -0700

    Updates

---
+++ precache-manifest.85dbb5d2b03d12b12c7a9528dcad6115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682651fb82cbe70279f79cc1d255977c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bda5c47cbf492a5a57c9",
+    "url": "/monsters-rolodex/static/css/main.74c93936.chunk.css"
+  },
+  {
+    "revision": "980790985778ce79a8be",
+    "url": "/monsters-rolodex/static/js/2.65fdaadb.chunk.js"
+  },
+  {
+    "revision": "e51d6b97aafea622679348b157ef81a5",
+    "url": "/monsters-rolodex/static/js/2.65fdaadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda5c47cbf492a5a57c9",
+    "url": "/monsters-rolodex/static/js/main.bb40cc91.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
