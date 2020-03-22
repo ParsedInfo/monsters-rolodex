cad346add36e77ff5f5b4e4e29fdcb9f1a3f6e9b
Jeff <jeff@parsed.info>
2020-03-21 20:12:12 -0700

    FirstCommit

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'acs1'
+        },
+        {
+          name: 'Dracula',
+          id: 'acs2'
+        },
+        {
+          name: 'Zombie',
+          id: 'acs3'
+        }
+      ]
+    }
+  }
+render() {
+  return (
+    <div className="App">
+     {this.state.monsters.map(monster => (
+         <h1 key={monster.id}> {monster.name}</h1>
+        ))}
+    </div>
+  );
+}
+}
+export default App;
